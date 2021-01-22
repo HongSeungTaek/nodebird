a6eab435c990e10af7deb4a00f68d7cab90a1540
홍승택 <tmdxor66@naver.com>
2021-01-22 23:26:11 +0900

    카카오 로그인 구현 로직 추가

--- routes/auth.js
+++ routes/auth.js
@@ -9,8 +9,8 @@ const router = express.Router();
 router.post('/join', isNotLoggedIn, async (req, res, next) => {
   const { email, nick, password } = req.body;
   try {
-    const exUser = await User.findOne({ where: { email }});
-    if(exUser) {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
       return res.redirect('/join?error=exist');
     }
     const hash = await bcrypt.hash(password, 12);
@@ -21,33 +21,41 @@ router.post('/join', isNotLoggedIn, async (req, res, next) => {
     });
     return res.redirect('/');
   } catch (error) {
-    console.log(error);
+    console.error(error);
     return next(error);
   }
 });
 
 router.post('/login', isNotLoggedIn, (req, res, next) => {
   passport.authenticate('local', (authError, user, info) => {
-    if(authError) {
+    if (authError) {
       console.error(authError);
-      return next();
+      return next(authError);
     }
-    if(!user) {
+    if (!user) {
       return res.redirect(`/?loginError=${info.message}`);
     }
     return req.login(user, (loginError) => {
-      if(loginError) {
+      if (loginError) {
         console.error(loginError);
         return next(loginError);
       }
       return res.redirect('/');
     });
-  })(req, res, next);
+  })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙입니다.
 });
 
 router.get('/logout', isLoggedIn, (req, res) => {
   req.logout();
   req.session.destroy();
+  res.redirect('/');
+});
+
+router.get('/kakao', passport.authenticate('kakao'));
+
+router.get('/kakao/callback', passport.authenticate('kakao', {
+  failureRedirect: '/',
+}), (req, res) => {
   res.redirect('/');
 });
 
